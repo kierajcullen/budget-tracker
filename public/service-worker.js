c7ab66dfff4284b55124ac1430878f51c7df0d3b
kierajcullen <kierajcullen@gmail.com>
2021-04-24 10:48:27 -0400

    Initial commit

---
+++ public/service-worker.js
@@ -0,0 +1,64 @@
+const FILES_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/styles.css",
+  "/index.js",
+  "/manifest.webmanifest",
+  "/db.js",
+];
+
+const DATA_CACHE_NAME = "data-cache-v1";
+const CACHE_NAME = "static-cache-v2";
+self.addEventListener("install", function (evt) {
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("Your files were pre-cached successfully!");
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+self.addEventListener("activate", function (evt) {
+  evt.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+            console.log("Removing old cache data", key);
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+self.addEventListener("fetch", function (evt) {
+  if (evt.req.url.includes("/api/")) {
+    evt.respondWith(
+      caches
+        .open(DATA_CACHE_NAME)
+        .then((cache) => {
+          return fetch(evt.req)
+            .then((res) => {
+              if (res.status === 200) {
+                cache.put(evt.req.url, res.clone());
+              }
+              return res;
+            })
+            .catch((err) => {
+              return cache.match(evt.req);
+            });
+        })
+        .catch((err) => console.log(err))
+    );
+    return;
+  }
+  evt.respondWith(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.match(evt.req).then((res) => {
+        return res || fetch(evt.req);
+      });
+    })
+  );
+});
