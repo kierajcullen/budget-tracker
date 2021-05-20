a7a2e611189d7b0a4804f4717a53f5dafc531a9e
kierajcullen <kierajcullen@gmail.com>
2021-05-20 01:59:30 -0400

    readme and database updates

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 ## Budget Tracker
 
-This application utilizes several libraries to create a responsive budget tracker for users. The application was deployed to Heroku and incorporates backend and front-end web development.
+This application utilizes several libraries to create a responsive budget tracker for users. The application was deployed to Heroku and incorporates backend and front-end web development. Users can enter deposits and expenses offline which will update their online tracker.
 
-##
+## Deployed Application
+
+![github](img/github.png)
--- public/service-worker.js
+++ public/service-worker.js
@@ -38,7 +38,6 @@ self.addEventListener("activate", function (evt) {
   self.clients.claim();
 });
 
-// fetch
 self.addEventListener("fetch", function (evt) {
   if (evt.request.url.includes("/api/")) {
     evt.respondWith(
@@ -47,7 +46,6 @@ self.addEventListener("fetch", function (evt) {
         .then((cache) => {
           return fetch(evt.request)
             .then((response) => {
-              // If the response was good, clone it and store it in the cache.
               if (response.status === 200) {
                 cache.put(evt.request.url, response.clone());
               }
@@ -55,7 +53,6 @@ self.addEventListener("fetch", function (evt) {
               return response;
             })
             .catch((err) => {
-              // Network request failed, try to get it from the cache.
               return cache.match(evt.request);
             });
         })
